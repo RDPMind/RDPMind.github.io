013a5c6ce888c08d12533bb6017213e3a89f7664
LeeSun <1853904118@qq.com>
2020-05-06 19:41:18 +0800

    Site updated: 2020-05-06 19:41:17

--- game/tkdz/js/num.js
+++
@@ -1,1 +0,0 @@
-"use strict";var Num=function(s){this.ctx=s,this.size=14,this.draw=function(s,i,t){var e=i,h=t,n=[];if(0==s)n.push(0);else for(;0<s;)n.push(s%10),s=parseInt(s/10);for(var r=n.length-1;0<=r;r--)e=i+(n.length-r-1)*this.size,this.ctx.drawImage(RESOURCE_IMAGE,POS.num[0]+14*n[r],POS.num[1],this.size,this.size,e,h,this.size,this.size)}};
